16df704db25a715f5b3bf5a6031bc48ffbb9daf8
KSigma91 <andrea19912014@gmail.com>
2022-07-29 14:46:05 +0200

    first commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>WIP</h1>
+    <!-- <h2>Sito in costruzione</h2> -->
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
